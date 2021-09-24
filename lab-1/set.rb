f6a9dcdf1a3b5e54827081ef6d62ffd0bcc7c801
Hunter Giffin <giffin.225@buckeyemail.osu.edu>
2021-09-24 13:20:33 -0400

    Added logic for adding, removing, and replacing cards.

--- lab-1/set.rb
+++ lab-1/set.rb
@@ -39,8 +39,9 @@ temp = []
 
 # Prompt the user to choose three cards then determine if they are a set
   gameOver = false
+  maxCards = 21
   until gameOver == true
-    puts "\nEnter three card numbers (numbers on far left of each row) that make a set."
+    puts "\nEnter three card numbers (numbers on far left of each row, with commas in between each number) that make a set."
     puts "If you see no valid sets, enter 'n' to recieve three more cards."
     input = gets.chomp
     if input == "n"
@@ -48,6 +49,8 @@ temp = []
       if deck.length == 0
         puts "Sorry there are no more cards. Game Over!"
         gameOver = true
+      elsif temp.length == maxCards
+        puts "Sorry, you can only have 21 cards out at a time!"
       else
         #reprint out all the cards from the previous round
         temp.each_with_index do |card, index|
@@ -63,14 +66,32 @@ temp = []
       end
     else
       #if string was not 'n', make a string only containing numbers the user input, convert to array
-      nums = input.scan(/\d/).join().chomp
-      numsArr = nums.scan(/\w/)
-      numsArr.map!(&:to_i)
+      nums = input.split(',')
+      #numsArr = nums.scan(/\w/)
+      nums.map!(&:to_i)
       #output error message if the user input less or more than 3 card numbers
-      if numsArr.length != 3
+      if nums.length != 3
         puts "ERROR: Make sure to enter exactly three card numbers!"
       else
-        puts Card.valid_set?(temp[numsArr[0]], temp[numsArr[1]], temp[numsArr[2]])
+        validSet = Card.valid_set?(temp[nums[0]], temp[nums[1]], temp[nums[2]])
+        puts validSet
+        if validSet
+          if temp.length <= 12
+            for i in 0..2
+              item = deck.sample
+              temp[nums[i]] = deck.delete(item)
+            end
+          else
+            temp.delete_at(nums[0])  
+            temp.delete_at(nums[1])  
+            temp.delete_at(nums[2])  
+          end
+
+          #reprint out all the cards from the previous round
+          temp.each_with_index do |card, index|
+            puts "#{index}  #{card.color}, #{card.number}, #{card.shape}, #{card.shading}"
+          end
+        end
       end
     end
   end
