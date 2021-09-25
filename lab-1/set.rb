04506adf8a4310b34a28d0a07962a4be3c5731fe
Nathan Chin <razer1@nathanchin.com>
2021-09-25 14:56:41 -0400

    Added basic multiplayer score system. Added a hint system. Reformatted card printing to be easier to read

--- lab-1/set.rb
+++ lab-1/set.rb
@@ -30,31 +30,36 @@ end
 # Main game code
 puts "Game Started"
 deck = create_deck
+playerA_score = 0
+playerB_score = 0
 
 # Randomly pick 12 cards from the deck
 totalCard = 0
-temp = []
+dealt_cards = []
 
   while totalCard < 12 
     item = deck.sample
-    puts "#{totalCard}  #{item.color}, #{item.number}, #{item.shape}, #{item.shading}"
-    temp << deck.delete(item)
+    puts "#{totalCard}\t#{item.color}\t#{item.number}\t#{item.shape}    \t#{item.shading}"
+    dealt_cards << deck.delete(item)
     totalCard = totalCard + 1
   end
 
 # Prompt the user to choose three cards then determine if they are a set
   gameOver = false
   maxCards = 21
+  puts "\nEnter the indices of three cards that make a set."
+  puts "Player A, type your anwer as \"A #,#,#\""
+  puts "Player B, type your anwer as \"B #,#,#\""
+  puts "If you see no valid sets, enter 'n' to recieve three more cards."
+  puts "If you need help, enter 'h' to view a hint."
   until gameOver == true
-    puts "\nEnter three card numbers (numbers on far left of each row, with commas in between each number) that make a set."
-    puts "If you see no valid sets, enter 'n' to recieve three more cards."
     input = gets.chomp
     if input == "n"
       #if there are no more cards in the deck, the game is over
       if deck.length == 0
         puts "Sorry there are no more cards. Game Over!"
         gameOver = true
-      elsif temp.length == maxCards
+      elsif dealt_cards.length == maxCards
         puts "\nSorry, you can only have 21 cards out at a time!"
         puts "Would you like to keep looking for a set? (Enter 'y' to keep looking or 'n' to end the game)."
         keepPlaying = ""
@@ -69,19 +74,23 @@ temp = []
         end
       else
         #reprint out all the cards from the previous round
-        temp.each_with_index do |card, index|
-          puts "#{index}  #{card.color}, #{card.number}, #{card.shape}, #{card.shading}"
+        dealt_cards.each_with_index do |card, index|
+          puts "#{index}\t#{card.color}\t#{card.number}\t#{card.shape}    \t#{card.shading}"
         end
         #print out three new random cards from the deck
         for i in 0..2
           item = deck.sample
-          puts "#{totalCard}  #{item.color}, #{item.number}, #{item.shape}, #{item.shading}"
-          temp << deck.delete(item)
+          puts "#{totalCard}\t#{item.color}\t#{item.number}\t#{item.shape}    \t#{item.shading}"
+          dealt_cards << deck.delete(item)
           totalCard = totalCard + 1
         end
       end
+    elsif input == "h"
+      set = find_set(dealt_cards)
+      puts "There is a card that forms a set with cards #{dealt_cards.find_index(set[0])} and #{dealt_cards.find_index(set[1])}. Good luck!"
     else
-      #if string was not 'n', make a string only containing numbers the user input, convert to array
+      #if string was not 'n' or 'h', make a string only containing numbers the user input, convert to array
+      playerID = input.slice! 0   # Remove the player ID from input while also recording which player found the set
       nums = input.split(',')
       #numsArr = nums.scan(/\w/)
       nums.map!(&:to_i)
@@ -89,25 +98,37 @@ temp = []
       if nums.length != 3
         puts "ERROR: Make sure to enter exactly three card numbers!"
       else
-        validSet = Card.valid_set?(temp[nums[0]], temp[nums[1]], temp[nums[2]])
-        puts validSet
+        validSet = Card.valid_set?(dealt_cards[nums[0]], dealt_cards[nums[1]], dealt_cards[nums[2]])
+        puts "\nCorrect. Good Job, Player #{playerID}." if  validSet
+        puts "\nIncorrect. Not a valid set. Try again!" if !validSet
+        puts
         if validSet
-          if temp.length <= 12
+                # Replace the cards that were selected
+          if dealt_cards.length <= 12
             for i in 0..2
               item = deck.sample
-              temp[nums[i]] = deck.delete(item)
+              dealt_cards[nums[i]] = deck.delete(item)
             end
-          else
+          else  # Remove the cards that were selected
             for i in 0..2
-              card = temp[nums[i]]
-              temp.delete(card)  
+              card = dealt_cards[nums[i]]
+              dealt_cards.delete(card)  
             end
           end
 
+          # Add 1 to the score of the player who found the set
+          playerA_score += 1 if playerID == "A"
+          playerB_score += 1 if playerID == "B"
+          puts "Invalid player ID entered; score not counted" if playerID != "A" && playerID != "B"
+
           #reprint out all the cards from the previous round
-          temp.each_with_index do |card, index|
-            puts "#{index}  #{card.color}, #{card.number}, #{card.shape}, #{card.shading}"
+          dealt_cards.each_with_index do |card, index|
+            puts "#{index}\t#{card.color}\t#{card.number}\t#{card.shape}    \t#{card.shading}"
           end
+
+          # Display current player scores
+          puts "\nPlayer A score: #{playerA_score}"
+          puts "Player B score: #{playerB_score}"
         end
       end
     end
