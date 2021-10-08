6f2aa055fc0eb51af9e9dfceebb8d51d0109373c
Hunter Giffin <giffin.225@buckeyemail.osu.edu>
2021-10-08 19:55:14 -0400

    added code to deselect player button

--- lab-2/pagescript.js
+++ lab-2/pagescript.js
@@ -1,23 +1,40 @@
 var currentPlayer = ''; // 'playerA' or 'playerB'
 var playerAScore = 0;
 var playerBScore = 0;
+var playerAButton = false;
+var playerBButton = false;
 
-document.getElementById('cards').addEventListener('click', function(event) {
+document.getElementById('cards').addEventListener('click', function (event) {
   if (event.target.nodeName === 'BUTTON') {
     window.alert(event.target.innerHTML);
   }
 });
 
-document.getElementById('players').addEventListener('click', function(event) {
+document.getElementById('players').addEventListener('click', function (event) {
   if (event.target.nodeName === 'BUTTON') {
-      event.target.style.color = 'green';
-      event.target.style.fontSize = 'large';
-      if (event.target.id === 'playerA') {
+    if (event.target.id === 'playerA') {
+      if (playerAButton == false) {
+        event.target.style.color = 'green';
+        event.target.style.fontSize = 'large';
+        playerAButton = true;
         currentPlayer = 'playerA';
       } else {
-        currentPlayer = 'playerB';
+        event.target.style.color = 'black';
+        event.target.style.fontSize = 'small'
+        playerAButton = false;
       }
-
+    } else {
+      if (playerBButton == false) {
+        event.target.style.color = 'green';
+        event.target.style.fontSize = 'large';
+        playerBButton = true;
+        currentPlayer = 'playerB';
+      } else {
+        event.target.style.color = 'black';
+        event.target.style.fontSize = 'small'
+        playerBButton = false;
+      }
+    }
   }
 });
 
