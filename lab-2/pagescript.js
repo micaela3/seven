4be8eeb8f12e30f3328d952871dac5a730559a31
Nathan Chin <razer1@nathanchin.com>
2021-10-07 23:37:12 -0400

    Changed Player1 and Player2 to PlayerA and PlayerB, respectively.

--- lab-2/pagescript.js
+++ lab-2/pagescript.js
@@ -1,4 +1,6 @@
-var currentPlayer = ''; // 'player1' or 'player2'
+var currentPlayer = ''; // 'playerA' or 'playerB'
+var playerAScore = 0;
+var playerBScore = 0;
 
 document.getElementById('cards').addEventListener('click', function(event) {
   if (event.target.nodeName === 'BUTTON') {
@@ -10,10 +12,10 @@ document.getElementById('players').addEventListener('click', function(event) {
   if (event.target.nodeName === 'BUTTON') {
       event.target.style.color = 'green';
       event.target.style.fontSize = 'large';
-      if (event.target.id === 'player1') {
-        currentPlayer = 'player1';
+      if (event.target.id === 'playerA') {
+        currentPlayer = 'playerA';
       } else {
-        currentPlayer = 'player2';
+        currentPlayer = 'playerB';
       }
 
   }
