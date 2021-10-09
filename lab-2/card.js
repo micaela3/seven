c8e6790e58b7165e1630aaf27299bd26555db218
Hunter Giffin <giffin.225@buckeyemail.osu.edu>
2021-10-08 21:50:11 -0400

    added deck class and new functions to card.js

--- lab-2/card.js
+++ lab-2/card.js
@@ -1,3 +1,28 @@
+const COLORS = ["red", "green", "purple"];
+const NUMBERS = ["1", "2", "3"];
+const SHAPES = ["~", "▢", "△"];
+const SHADING = ["solid", "striped", "open"];
+
+export default class Deck {
+  constructor(cards = newDeck()) {
+    this.cards = cards
+  }
+
+  // Taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffle() {
+    let currentIndex = this.cards.length, randomIndex;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // And swap it with the current element.
+      [this.cards[currentIndex], this.cards[randomIndex]] = [
+        this.cards[randomIndex], this.cards[currentIndex]];
+    }
+  }
+}
+
 class Card {
   constructor(color, number, shape, shading) {
     this.color = color
@@ -5,23 +30,35 @@ class Card {
     this.shape = shape
     this.shading = shading
   }
+}
 
-  isValidSet(card1, card2, card3) {
-    cardArr = [card1, card2, card3]
+function newDeck() {
+  return COLORS.flatMap(color => {
+    return NUMBERS.flatMap(number => {
+      return SHAPES.flatMap(shape => {
+        return SHADING.map(shading => {
+          return new Card(color, number, shape, shading);
+        })
+      })
+    })
+  })
+}
 
-    if (!(card1.color === card2.color && card2.color === card3.color) || !(card1.color !== card2.color && card1.color !== card3.color && card2.color != card3.color)) {
-      return false;
-    }
-    if (!(card1.number === card2.number && card2.number === card3.number) || !(card1.number !== card2.number && card1.number !== card3.number && card2.number != card3.number)) {
-      return false;
-    }
-    if (!(card1.shape === card2.shape && card2.shape === card3.shape) || !(card1.shape !== card2.shape && card1.shape !== card3.shape && card2.shape != card3.shape)) {
-      return false;
-    }
-    if (!(card1.shading === card2.shading && card2.shading === card3.shading) || !(card1.shading !== card2.shading && card1.shading !== card3.shading && card2.shading != card3.shading)) {
-      return false;
-    }
+function isValidSet(card1, card2, card3) {
+  cardArr = [card1, card2, card3]
 
-    return true;
+  if (!(card1.color === card2.color && card2.color === card3.color) || !(card1.color !== card2.color && card1.color !== card3.color && card2.color != card3.color)) {
+    return false;
   }
+  if (!(card1.number === card2.number && card2.number === card3.number) || !(card1.number !== card2.number && card1.number !== card3.number && card2.number != card3.number)) {
+    return false;
+  }
+  if (!(card1.shape === card2.shape && card2.shape === card3.shape) || !(card1.shape !== card2.shape && card1.shape !== card3.shape && card2.shape != card3.shape)) {
+    return false;
+  }
+  if (!(card1.shading === card2.shading && card2.shading === card3.shading) || !(card1.shading !== card2.shading && card1.shading !== card3.shading && card2.shading != card3.shading)) {
+    return false;
+  }
+
+  return true;
 }
--- lab-2/pagescript.js
+++ lab-2/pagescript.js
@@ -1,3 +1,10 @@
+import Deck from "./card.js"
+
+const deck = new Deck();
+deck.shuffle();
+console.log('test');
+console.log(deck.cards);
+
 var currentPlayer = ''; // 'playerA' or 'playerB'
 var playerAScore = 0;
 var playerBScore = 0;
@@ -13,23 +20,24 @@ document.getElementById('cards').addEventListener('click', function (event) {
 document.getElementById('players').addEventListener('click', function (event) {
   if (event.target.nodeName === 'BUTTON') {
     if (event.target.id === 'playerA') {
+      //if playerA is off, turn it on
       if (playerAButton == false) {
         event.target.style.color = 'green';
         event.target.style.fontSize = 'large';
         playerAButton = true;
         currentPlayer = 'playerA';
-      } else {
+      } else { //if playerA is on, turn it off
         event.target.style.color = 'black';
         event.target.style.fontSize = 'small'
         playerAButton = false;
       }
-    } else {
-      if (playerBButton == false) {
+    } else { 
+      if (playerBButton == false) { //if playerB is off, turn it on
         event.target.style.color = 'green';
         event.target.style.fontSize = 'large';
         playerBButton = true;
         currentPlayer = 'playerB';
-      } else {
+      } else { //if playerB is on, turn it off
         event.target.style.color = 'black';
         event.target.style.fontSize = 'small'
         playerBButton = false;
