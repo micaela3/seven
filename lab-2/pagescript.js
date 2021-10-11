43d1a348584da1bb6e0c4621b248a43bb486e3f7
Sanjana Panjala <panjala.2@osu.edu>
2021-10-11 00:41:28 -0400

    final pagescript.js with comments added

--- lab-2/pagescript.js
+++ lab-2/pagescript.js
@@ -4,26 +4,24 @@ const SQUARES = ["■", "◧", "□"];
 
 //Create a new deck and shuffle it
 const deck = new Deck();
-deck.shuffle();
-
-console.log(deck.cards);
+deck.shuffle(); //shuffle the deck so the cards are randomized
 
 var currentPlayer = ''; // 'playerA' or 'playerB'
 var playerAScore = 0;
 var playerBScore = 0;
-var playerAButton = false;
+var playerAButton = false; //true if playerA/B button has been pressed
 var playerBButton = false;
-var numCardsOnTable = 0;
-var totalCardsOut = 0;
-var numRows = 3;
-
-console.log(deck.cards);
+var numCardsOnTable = 0; //current number of cards on the table
+var totalCardsOut = 0; //total number of cards that have been dealt
+var numRows = 3; //number of rows of cards that are out
 
 //Display the values on the initial twelve cards
 var initialCards = document.querySelectorAll('button.card');
+//iterate through all card buttons
 for (var i = 0; i < initialCards.length; i++) {
   numCardsOnTable++;
   totalCardsOut++;
+  //change the card.shape to the appropriate symbol
   switch (deck.cards[i].shape) {
     case "triangle":
       if (deck.cards[i].shading === "solid") {
@@ -53,21 +51,15 @@ for (var i = 0; i < initialCards.length; i++) {
         deck.cards[i].shape = SQUARES[2];
       }
   }
+  //add a class to the card named after its color
   initialCards[i].classList.add(`${deck.cards[i].color}`);
-  // initialCards[i].innerHTML = "";
+  //display the shapes on the html cards
   for (var j = 0; j < deck.cards[i].number; j++) {
     initialCards[i].innerHTML = initialCards[i].innerHTML.concat(`<br>${deck.cards[i].shape}\n`);
   }
 }
 
-
-document.getElementById('cards').addEventListener('click', function (event) {
-  if (event.target.nodeName === 'BUTTON') {
-    window.alert(event.target.innerHTML);
-  }
-});
-
-
+//function for identifying if a player's button has been activated
 document.getElementById('players').addEventListener('click', function (event) {
   if (event.target.nodeName === 'BUTTON') {
     if (event.target.id === 'playerA') {
@@ -93,28 +85,28 @@ document.getElementById('players').addEventListener('click', function (event) {
   }
 });
 
-// adding event listeners for hint button, draw cards button, and submit set guess button
-// document.getElementById('hintButton').addEventListener('click', hint);
+//function for drawing three cards when the 'Draw More Cards' button is pressed
 document.getElementById('moreCardsButton').addEventListener('click', drawCards);
-
 function drawCards(event) {
   if (numCardsOnTable == 21) {
     alert("You can't have more than 21 cards out at a time!")
-  } else if (deck.cards[80].shape.length === 1) {
+  } else if (deck.cards[80].shape.length === 1) { //if last card in deck has been changed, deck is empty
        alert("There are no more cards in the deck!")
   } else {
-    // Find a <table> element with id="myTable":
+    // Access the table set up in the HTML file
     var table = document.getElementById("cards");
-    // Create an empty <tr> element and add it to the 1st position of the table:
+    // Create an empty <tr> element and add it to the bottom of the table:
     var row = table.insertRow(numRows);
     numRows++;
-    // Add some text to the new cells:
+    // Add the next three cards to the new row
     var index = 0;
     for (var cardsOnTable = numCardsOnTable; numCardsOnTable < cardsOnTable + 3; i++) {
       var tableCell = row.insertCell(index);
-      // tableCell.setAttribute('id', 'newCard');
+      // give the new card a unique id
       tableCell.setAttribute('id', `card${totalCardsOut}`)
+      // give the new card the same class as the original cards
       tableCell.classList.add("card");
+      // change the card.shape to the appropriate symbol
       switch (deck.cards[i].shape) {
         case "triangle":
           if (deck.cards[i].shading === "solid") {
@@ -144,8 +136,11 @@ function drawCards(event) {
             deck.cards[i].shape = SQUARES[2];
           }
       }
+      // add a color class to the new card
       tableCell.classList.add(`${deck.cards[i].color}`);
+      // display text of the card name on the new card
       tableCell.innerHTML = "Card".concat(totalCardsOut);
+      // display the card shapes on the new cards
       for (var j = 0; j < deck.cards[i].number; j++) {
         tableCell.innerHTML = tableCell.innerHTML.concat(`<br>${deck.cards[i].shape}\n`);
       }
@@ -156,37 +151,26 @@ function drawCards(event) {
   }
 };
 
+// Function to remove a card from the table
 function removeCard(ID) {
   let cardElement = document.getElementById(ID);
-  console.log(ID);
   cardElement.remove();
   numCardsOnTable--;
 }
 
-/*function replaceCards(cardNum) {
-  let cardElement = document.getElementById('card'+ cardNum);
-  cardElement.innerHTML = deck.cards[numCardsOnTable+1];
-}*/
-
-// document.getElementById('moreCardsButton').addEventListener('click', drawMore);
-// alter set.js so that drawMore is its own function instead of in while loop
-
-document.getElementById('submitSetButton').addEventListener('click', submitSetGuess);
-// alter set.js so that submitSetGuess is its own function instead of in while loop
-
-/* submitSetGuess would be a function that calls other functions to:
+/* submitSetGuess is a function that calls other functions to:
  *1.  check whether the input values in the textfields form a valid set
  * (get player's guesses from text fields by var cardNum1 = document.getElementById("card1").value;
  *2.  update the score if needed
  *3.  deal more cards if needed*/
+document.getElementById('submitSetButton').addEventListener('click', submitSetGuess);
 function submitSetGuess(event) {
   // Get the cards that were entered by user in text fields
   var card1 = deck.cards[document.getElementById('choice1').value];
   var card2 = deck.cards[document.getElementById('choice2').value];
   var card3 = deck.cards[document.getElementById('choice3').value];
 
-  console.log(card1, card2, card3);
-
+  //Call function to check if the user entered a valid set
   var isValid = isValidSet(card1, card2, card3);
 
   // If valid, increment the appropriate player's score
@@ -197,10 +181,12 @@ function submitSetGuess(event) {
     // Check if score is 3. If so, this player wins.
     if (currentScore == 2) {
       alert(`${currentPlayer} has won the game!`);
+      // since the game is over, reload the page
       window.location.reload();
       return;
     }
     currentScore += 1;
+    //adjust the score element in the HTML file
     scoreElement.innerHTML = currentScore;
 
     // Remove cards that were chosen
@@ -227,4 +213,6 @@ function submitSetGuess(event) {
   // Set currentPlayer back to none (empty string)
   currentPlayer = '';
 }
+
+
 
