cbd662370720e496c3c7d00e8d1394b1a800d33b
Nathan Chin <razer1@nathanchin.com>
2021-10-22 17:34:58 -0400

    Added code to locate the specific attributes we want in the html. Not all are written - WIP

--- lab-3/web_scraper/app/models/course.rb
+++ lab-3/web_scraper/app/models/course.rb
@@ -1,2 +1,23 @@
 class Course < ApplicationRecord
+  @name = 'course'
+  @engine= :mechanize
+
+  def self.process(url)
+    @start_urls = [url]
+    self.crawl!
+  end
+
+  def parse(response, url:, data: {})
+    response.xpath("//duv[@class=course']").each do |course|
+      item = {}
+    item[:course_name] = course.css('h3.ng-binding')&.text&.squish
+    item[:sections]    = course.css('span.lightweight')&.text&.squish
+    # TODO: class_type
+    # TODO: autoenroll
+    # TODO: weekdays
+    # TODO: time
+    # TODO: seating
+    # TODO: instructor
+
+  end
 end
