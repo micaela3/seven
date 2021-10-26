cfa59c2e39ad09d600e65b5a91660d854346207c
Nathan Chin <razer1@nathanchin.com>
2021-10-26 17:38:35 -0400

    Got the attribute course_name to be added to the table of scraped data by refactoring and switching to CSS selectors

--- lab-3/web_scraper/app/models/courses_spider.rb
+++ lab-3/web_scraper/app/models/courses_spider.rb
@@ -12,25 +12,23 @@ class CoursesSpider < Kimurai::Base
   def parse(response, url:, data: {})
     # sleep 2
     # response = browser.current_response
-    Rails.logger.info "DEBUG: response=#{response}"
-    response.xpath("//div[@class='course']").each do |course|
+    # Rails.logger.info "DEBUG: response=#{response}"
+    # temp = response.xpath("//div[@class='course']")
+    # response.xpath("//div[@class='course']").each do |course|
+    response.css('div.course').each do |course|
       item = {}
-      Rails.logger.info "IN COURSE CLASS"
+      # Rails.logger.info "DEBUG: course attribute title: #{course.css('h3.ng-binding').text}"
       #Rails.logger.info "#{response}"
-      Rails.logger.info "COURSE NAME: #{response.xpath("//span[@class='course-title']/*").text.squish}"
-      #item[:course_name] = response.xpath("//span[@class='course-title']/*").text
-      #item[:course_name] = course.css('//*[@id="main"]/div/div[4]/div[2]/div[1]/div[1]/div[1]/div/h3/text()')&.text&.squish
-      #iterm[:course_name] = course.css('h3.ng-binding')&.text&.squish
-      #item[:section]       = course.css('span.lightweight')&.text&.squish&.delete('^0-9').to_i
-      #item[:class_type]   = course.css('span.sr-only sr-only-focusable ng-binding')&.text&.squish
-      #item[:autoenroll]   = course.css('span.listing-row__mileage')&.text&.squish&.delete('^0-9').to_i
-      #item[:weekdays]     = course.css('ul.listing-row__meta li')[0]&.text&.squish.gsub('Ext. Color: ', '')
-      #item[:time]         = course.css('ul.listing-row__meta li')[1]&.text&.squish.gsub('Int. Color: ', '')
-      #item[:course_id]    = course.css('ul.listing-row__meta li')[2]&.text&.squish.gsub('Transmission: ', '')
-      #item[:seating]      = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
-      #item[:instructor]   = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
+      item[:course_name] = course.css('h3.ng-binding')&.text&.squish
+      # item[:section]       = course.css('span.lightweight')&.text&.squish&.delete('^0-9').to_i
+      # item[:class_type]   = course.css('span.sr-only sr-only-focusable ng-binding')&.text&.squish
+      # item[:autoenroll]   = course.css('span.listing-row__mileage')&.text&.squish&.delete('^0-9').to_i
+      # item[:weekdays]     = course.css('ul.listing-row__meta li')[0]&.text&.squish.gsub('Ext. Color: ', '')
+      # item[:time]         = course.css('ul.listing-row__meta li')[1]&.text&.squish.gsub('Int. Color: ', '')
+      # item[:course_id]    = course.css('ul.listing-row__meta li')[2]&.text&.squish.gsub('Transmission: ', '')
+      # item[:seating]      = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
+      # item[:instructor]   = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
       Course.where(item).first_or_create
-      #save_to ""
     end
   end
 end
