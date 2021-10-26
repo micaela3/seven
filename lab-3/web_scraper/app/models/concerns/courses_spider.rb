038b90b4ff42e8f107ac415826a6c6329760592d
Hunter Giffin <giffin.225@buckeyemail.osu.edu>
2021-10-26 16:22:30 -0400

    Added user model with login/logout functionality

--- lab-3/web_scraper/app/models/concerns/courses_spider.rb
+++ lab-3/web_scraper/app/models/concerns/courses_spider.rb
@@ -1,33 +1,35 @@
 #code inspired from https://medium.com/swlh/web-scraper-application-with-ruby-on-rails-864dfaae6270
+require 'kimurai'
 class CoursesSpider < Kimurai::Base
-    @name = 'courses_spider'
-    @engine = :mechanize
-  
-    def self.process(url)
-      @start_urls = [url]
-      self.crawl!
-    end
-  
-    def parse(response, url:, data: {})
-      #response.xpath("//div[@class='course ng-scope']").each do |course|
-        #var course_name = course.css('h3.ng-binding')&.text&.squish
-        #console.log(course_name)
-        response.xpath("//div[@class='section-container ng-scope']").each do |course|
+  @name = 'courses_spider'
+  @engine = :mechanize
 
-            item = {}
-  
-            #item[:course_name]  = course_name
-            item[:section]      = course.css('span.lightweight ng-binding')&.text&.squish
-            #item[:class_type]   = course.css('span.sr-only sr-only-focusable ng-binding')&.text&.squish
-            #item[:autoenroll]   = course.css('span.listing-row__mileage')&.text&.squish&.delete('^0-9').to_i
-            #item[:weekdays]     = course.css('ul.listing-row__meta li')[0]&.text&.squish.gsub('Ext. Color: ', '')
-            #item[:time]         = course.css('ul.listing-row__meta li')[1]&.text&.squish.gsub('Int. Color: ', '')
-            #item[:course_id]    = course.css('ul.listing-row__meta li')[2]&.text&.squish.gsub('Transmission: ', '')
-            #item[:seating]      = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
-            #item[:instructor]   = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
+  def self.process(url)
+    @start_urls = [url]
+    self.crawl!
+  end
 
-            Course.where(item).first_or_create
-        #end
-      end
+  def parse(response, url:, data: {})
+    Rails.logger.info "URL: #{url}"
+    #response.xpath("/html/body/div[3]/div/div/div/div[4]/div[2]/div[1]/div[1]").each do |course|
+    response.xpath("//div[@class='course']").each do |course|
+      item = {}
+      Rails.logger.info "IN COURSE CLASS"
+      #Rails.logger.info "#{response}"
+      Rails.logger.info "COURSE NAME: #{response.xpath("//span[@class='course-title']/*").text.squish}"
+      #item[:course_name] = response.xpath("//span[@class='course-title']/*").text
+      #item[:course_name] = course.css('//*[@id="main"]/div/div[4]/div[2]/div[1]/div[1]/div[1]/div/h3/text()')&.text&.squish
+      #iterm[:course_name] = course.css('h3.ng-binding')&.text&.squish
+      #item[:section]       = course.css('span.lightweight')&.text&.squish&.delete('^0-9').to_i
+      #item[:class_type]   = course.css('span.sr-only sr-only-focusable ng-binding')&.text&.squish
+      #item[:autoenroll]   = course.css('span.listing-row__mileage')&.text&.squish&.delete('^0-9').to_i
+      #item[:weekdays]     = course.css('ul.listing-row__meta li')[0]&.text&.squish.gsub('Ext. Color: ', '')
+      #item[:time]         = course.css('ul.listing-row__meta li')[1]&.text&.squish.gsub('Int. Color: ', '')
+      #item[:course_id]    = course.css('ul.listing-row__meta li')[2]&.text&.squish.gsub('Transmission: ', '')
+      #item[:seating]      = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
+      #item[:instructor]   = course.css('ul.listing-row__meta li')[3]&.text&.squish.gsub('Drivetrain: ', '')
+      Course.where(item).first_or_create
+      #save_to ""
     end
   end
+end
--- lab-3/web_scraper/config/routes.rb
+++ lab-3/web_scraper/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :courses do
     match '/scrape', to: 'courses#scrape', via: :post, on: :collection
   end
