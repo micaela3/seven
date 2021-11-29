52323a2dd7c056d3c109f4e2522c31351c3f5d5c
Nathan Chin <razer1@nathanchin.com>
2021-11-29 00:15:29 -0500

    Created a new homepage with links to pages for student, instructor, and admin. Added empty models and controllers for each new role type

--- lab-4/web_scraper/config/routes.rb
+++ lab-4/web_scraper/config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'admin/index'
+  get 'instructor/index'
+  get 'student/index'
+  get 'courses/index'
+
+  # Links for buttons on homepage
+  # resources :pages do
+  #   collection do
+  #     get :temp
+  #   end
+  # end
+
+  # Actions for buttons on courses page
   resources :courses do
     # Route 'Scrape' button on page to main scrape functionality
     match '/scrape', to: 'courses#scrape', via: :post, on: :collection
@@ -7,6 +20,6 @@ Rails.application.routes.draw do
     # Route 'Clear Table' button on page to method that clears & refreshes
     match '/clear_table', to: 'courses#clear_table', via: :post, on: :collection
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'courses#index'
+  # root to: 'courses#index'
+  root to: 'pages#home'
 end
