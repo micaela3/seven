b8886ac232ed8077593bc214ed21b50b41b814c6
Hunter Giffin <giffin.225@buckeyemail.osu.edu>
2021-11-29 20:16:52 -0500

    Fixed student application submission and updated styling

--- lab-4/web_scraper/app/controllers/student_application_controller.rb
+++ lab-4/web_scraper/app/controllers/student_application_controller.rb
@@ -2,4 +2,17 @@ class StudentApplicationController < ApplicationController
   def new
     @student_application = StudentApplication.new
   end
+  def create
+    @student_application = StudentApplication.new(user_params)
+    if @student_application.save
+      redirect_to root_path, notice: "Successfully Submitted Application"
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:student_application).permit(:email, :phone_number, :desired_courses, :taken_courses, :availability)
+  end
 end
--- lab-4/web_scraper/config/routes.rb
+++ lab-4/web_scraper/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "student_application/new"
+  get "student_application/new", to: "student_application#new"
   post "student_application/new", to: "student_application#create"
   devise_for :users
   get 'admin/index'
